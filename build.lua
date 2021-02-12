63f9625c52926bc9117002012e9a52ee187056c0
meolalala <57823331+meolalala@users.noreply.github.com>
2021-02-12 05:10:19 -0500

    Update build.lua

--- build.lua
+++ build.lua
@@ -2,10 +2,12 @@ local robot = require "robot"
 local comp = require "component"
 local sides = require "sides"
 
-function sneakPlaceUp()
+function placeUp()
     print("being a nyanja while placing blocks..")
-    robot.placeUp(sides: "top" sneaky: true)
+    robot.placeUp()
     print("placed block!")
 end
+
+placeUp()
 
 print("sleep time..")
